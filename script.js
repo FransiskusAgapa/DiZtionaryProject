4d9fb1655f144e3dcd878819a237ce7d895fef48
FransXave <santhothosa@gmail.com>
2023-07-31 16:01:04 -0700

    first word added

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let textBox = document.querySelector(".text-show");
+let textBox = document.querySelector(".text-show-box");
 
 function remove() {
   textBox.remove();
+}
+
+function addShadow(element) {
+  element.classList.add("shadow");
+}
+
+function removeShadow(element) {
+  element.classList.remove("shadow");
 }
